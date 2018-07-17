601fe184558c566e7a2197039aa3463f258093bd
prashantgupta0206 <prashantgupta0206@gmail.com>
2018-07-17 22:40:39 +0530

    Prashant: changes to update node packages

--- index.js
+++ index.js
@@ -1,9 +1,22 @@
-var http = require('http');
-var webDriver = require('selenium-webdriver');
-var By = require('selenium-webdriver').By;
-var until = require('selenium-webdriver').until;
+// var http = require('http');
+// var webDriver = require('selenium-webdriver');
+// var By = require('selenium-webdriver').By;
+// var until = require('selenium-webdriver').until;
+const {Builder, By, Key, until} = require('selenium-webdriver');
 var path = require('chromedriver').path;
 
 
-var browser = new webDriver.Builder().withCapabilities(webDriver.Capabilities.chrome()).build();
-browser.get('https://www-review-k6.thinkcentral.com/content/hsp/math/gomath2018/ny/gr3/transition_lessons_te_9781328595836_/index.html')
+// var browser = new webDriver.Builder().withCapabilities(webDriver.Capabilities.chrome()).build();
+// browser.get('https://www.google.co.in');
+var input = "google";
+
+(async function example() {
+    let driver = await new Builder().forBrowser('chrome').build();
+    try {
+      await driver.get('http://www.google.com/');
+      await driver.findElement(By.name('q')).sendKeys(input, Key.RETURN);
+      await driver.wait(until.titleIs('webdriver - Google Search'), 1000);
+    } finally {
+      //await driver.quit();
+    }
+  })();
